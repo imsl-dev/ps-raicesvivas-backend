67c7e90b570fd5bc06a4765fa8d51cfb7b36a410
juanbauza00 <juanpablobauza01@gmail.com>
2025-09-23 21:03:12 -0300

    Deleted CuentaBancariaRepository duplicated and added swagger configs to application.yml

--- src/main/java/com/raicesvivas/backend/models/repositories/CuentaBancariaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.raicesvivas.backend.models.repositories;
-
-import com.raicesvivas.backend.models.entities.auxiliar.CuentaBancaria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CuentaBancariaRepository extends JpaRepository<CuentaBancaria, Integer> {
-}
