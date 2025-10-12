a18dcfa043fd4a43c18f6e3763a8bec3a04f24b0
juanbauza00 <juanpablobauza01@gmail.com>
2025-10-12 19:27:22 -0300

    Se agregó EventoScheduledTask para el manejo automático de inicio y fin de eventos según hora/fecha

--- src/main/java/com/raicesvivas/backend/repositories/EventoRepository.java
+++ src/main/java/com/raicesvivas/backend/repositories/EventoRepository.java
@@ -1,12 +1,16 @@
 package com.raicesvivas.backend.repositories;
 
 import com.raicesvivas.backend.models.entities.Evento;
+import com.raicesvivas.backend.models.enums.EstadoEvento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Integer> {
     List<Evento> findByOrganizadorId(int id);
+    List<Evento> findByEstadoAndHoraInicioLessThanEqual(EstadoEvento estado, LocalDateTime fecha);
+    List<Evento> findByEstadoAndHoraFinLessThanEqual(EstadoEvento estado, LocalDateTime fecha);
 }
