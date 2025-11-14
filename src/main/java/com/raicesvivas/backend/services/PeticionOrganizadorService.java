525d4ec8d9704d88cd5bb6af77e92601be854196
Ignacio Solis <solis.luna.ignacio@gmail.com>
2025-11-13 21:55:54 -0300

    change peticion service

--- src/main/java/com/raicesvivas/backend/services/PeticionOrganizadorService.java
+++ src/main/java/com/raicesvivas/backend/services/PeticionOrganizadorService.java
@@ -2,8 +2,11 @@ package com.raicesvivas.backend.services;
 
 import com.raicesvivas.backend.models.dtos.PeticionOrganizadorDTO;
 import com.raicesvivas.backend.models.entities.PeticionOrganizador;
+import com.raicesvivas.backend.models.entities.Usuario;
 import com.raicesvivas.backend.models.enums.EstadoPeticion;
 import com.raicesvivas.backend.repositories.PeticionOrganizadorRepository;
+import com.raicesvivas.backend.repositories.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.modelmapper.ModelMapper;
@@ -16,6 +19,8 @@ public class PeticionOrganizadorService {
     private final ModelMapper mapper;
 
     private final PeticionOrganizadorRepository peticionRepository;
+
+    private final UsuarioRepository usuarioRepository;
 
     public boolean usuarioTienePeticion(Integer idUsuario) {
         PeticionOrganizador peticion = peticionRepository.findByUsuarioId(idUsuario);
@@ -34,11 +39,20 @@ public class PeticionOrganizadorService {
       peticion.setId(null);
       peticion.setEstadoPeticion(EstadoPeticion.PENDIENTE);
 
+        //fetch name by id
+        Usuario usuario = usuarioRepository.findById(dto.getIdUsuario()).orElseThrow(()
+                -> new EntityNotFoundException("Usuario no encontrado"));
+
+        peticion.setNombreUsuario(usuario.getNombre());
+        peticion.setApellidoUsuario(usuario.getApellido());
+        peticion.setEmail(usuario.getEmail());
+
       return peticionRepository.save(peticion);
     }
 
     public PeticionOrganizador getPeticionByUserId(Integer userId) {
         return peticionRepository.findByUsuarioId(userId);
+
     }
 
 
